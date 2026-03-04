e96dd693957a622ae61d12c862dabeca2b244258
xiaorui <rshen6906@gmail.com>
2026-03-04 11:10:23 +0800

    init(porject): Project Initialization

---
+++ src/main/java/com/xiaorui/youyouerpsystem/YouyouErpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.xiaorui.youyouerpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouyouErpSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouyouErpSystemApplication.class, args);
+    }
+
+}
